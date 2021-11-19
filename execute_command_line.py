31b78d00366168e0017eafc41b08bde3609fd476
Khai Duong <duongkhai@gmail.com>
2021-11-20 00:47:50 +0100

    Implement command for binary to decimal conversion

--- execute_command_line.py
+++ execute_command_line.py
@@ -11,10 +11,13 @@ def execute_command_line(argv: List[str] = ...):
     # Print help msg if no arguments were given
     if len(sys.argv) == 1:
         print("Missing commands. type dinex -h for help")
+        exit()
 
     # Command for converting decimal to binary values
-    parser.add_argument("-db", "--DecimalToBinary", metavar="", type=str,
-                        help="Enter integers or an IPv4 address")
+    parser.add_argument("-db", "--decimal-to-binary", metavar="", type=str,
+                        help="convert decimal to binary values")
+    parser.add_argument("-bd", "--binary-to-decimal", metavar="",
+                        type=str, help="convert binary to decimal values")
 
     # Verbose and quiet functionalities. They are mutually exclusive
     vq_group = parser.add_mutually_exclusive_group()
@@ -26,11 +29,20 @@ def execute_command_line(argv: List[str] = ...):
     # Store user input in a variable
     args = parser.parse_args()
 
-    # Logic for decimal to binary conversion
-    if args.DecimalToBinary is not None:
-        # Import module here, otherwise the logic in conv is executed automatically
-        from dinex_modules import Converter
+    # Import dinex modules only after everything is setup and ready. This is for
+    # performance optimization
+    from dinex_modules import Converter
 
-        con = Converter()
-        con.dec_to_bin(args.DecimalToBinary)
+    con = Converter()
+
+    # Logic for decimal to binary conversion
+    if args.decimal_to_binary is not None:
+        # Import module here, otherwise the logic in conv is executed automatically
+
+        con.dec_to_bin(args.decimal_to_binary)
+        con.results()
+
+    # Logic for binary to decimal conversion
+    if args.binary_to_decimal is not None:
+        con.bin_to_dec(args.binary_to_decimal)
         con.results()
